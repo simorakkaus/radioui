1d5ea420942c1a40434524d6ec0066556db61659
Simo <simo@mbp-simo.dlink>
2017-02-16 19:17:10 +0300

    Initial Commit

---
+++ RadioUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RadioUI
+//
+//  Created by Simo on 16.02.17.
+//  Copyright © 2017 Simo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
